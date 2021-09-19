68f750754e3938ed9269a670c1380f9671beb92f
mdnhs <mdnhs.cse@gmail.com>
2021-09-19 16:10:16 +0600

    third  commit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 window.addEventListener("load",()=>{
     document.querySelector(".main").classList.remove("hidden");
     document.querySelector(".home-section").classList.add("active");
-    //page oader
+    //page loader
     document.querySelector(".page-loader").classList.add("fade-out");
     setTimeout(()=>{
         document.querySelector(".page-loader").style.display="none";
